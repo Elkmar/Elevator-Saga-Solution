6b9aa9d55d8fc50d6246834a55ce15fd26cf701c
Elkmar <dimitri.jeangerard@gmail.com>
2023-04-18 15:39:05 +0100

    added a check to stop along the way if needed

--- script.js
+++ script.js
@@ -6,16 +6,18 @@
         var internalQueue = [];
         var externalQueue = [];
 
+        //if there is internal calls, process them first, then external calls, then go to the ground floor
         function processNextDestination() {
             if (internalQueue.length > 0) {
                 elevator.goToFloor(internalQueue.shift());
             } else if (externalQueue.length > 0) {
                 elevator.goToFloor(externalQueue.shift());
             } else {
-                elevator.goToFloor(2);
+                elevator.goToFloor(0);
             }
         }
 
+        //add external calls to the queue if they are not already there
         floors.forEach(function(floor) {
             floor.on("up_button_pressed down_button_pressed", function() {
                 if (!externalQueue.includes(floor.floorNum())) {
@@ -31,6 +33,7 @@
             processNextDestination();
         });
 
+        //if a customer press a button inside the elevator, add the floor to the queue if it's not already there
         elevator.on("floor_button_pressed", function(floorNum) {
             if (!internalQueue.includes(floorNum)) {
                 internalQueue.push(floorNum);
@@ -39,6 +42,14 @@
                 processNextDestination();
             }
         });
+        
+        //if the elevator is passing a floor where a stop is needed, stop there
+        elevator.on("passing_floor", function(floorNum, direction) {
+            if (internalQueue.includes(floorNum)) {
+                elevator.goToFloor(floorNum, true);
+            }
+         });
+
 
     },
     update: function(dt, elevators, floors) {
