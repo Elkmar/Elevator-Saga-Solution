ff06d6fa88b7a2a133078d1e4760a28e98154171
Elkmar <dimitri.jeangerard@gmail.com>
2023-04-20 12:59:08 +0100

    added multi elevators, challenge 4 completed

--- script.js
+++ script.js
@@ -1,57 +1,81 @@
 {
     init: function(elevators, floors) {
-        console.clear();
-        var elevator = elevators[0]; // Let's use the first elevator
+        console.clear(); 
 
-        var internalQueue = [];
-        var externalQueue = [];
+        // Initialize variables for each elevator's queues
+        var queues = [];
+        for (var i = 0; i < elevators.length; i++) {
+            queues[i] = {
+                internal: [],
+                external: []
+            };
+        }
 
-        //if there is internal calls, process them first, then external calls, then go to the ground floor
-        function processNextDestination() {
-            if (internalQueue.length > 0) {
-                elevator.goToFloor(internalQueue.shift());
-            } else if (externalQueue.length > 0) {
-                elevator.goToFloor(externalQueue.shift());
+        // Process the next destination for a given elevator
+        function processNextDestination(elevator, queue) {
+            if (queue.internal.length > 0) {
+                elevator.goToFloor(queue.internal.shift());
+            } else if (queue.external.length > 0) {
+                elevator.goToFloor(queue.external.shift());
             } else {
                 elevator.goToFloor(0);
             }
         }
 
-        //add external calls to the queue if they are not already there
+        // Add external calls to the appropriate elevator's queue
         floors.forEach(function(floor) {
             floor.on("up_button_pressed down_button_pressed", function() {
-                if (!externalQueue.includes(floor.floorNum())) {
-                    externalQueue.push(floor.floorNum());
+                var minDist = Number.MAX_VALUE;
+                var closestElevator;
+                queues.forEach(function(queue, i) {
+                    var dist = elevators[i].destinationQueue.length + queue.external.length;
+                    if (dist < minDist) {
+                        minDist = dist;
+                        closestElevator = i;
+                    }
+                });
+                var queue = queues[closestElevator];
+                if (!queue.external.includes(floor.floorNum())) {
+                    queue.external.push(floor.floorNum());
                 }
-                if (elevator.destinationQueue.length === 0) {
-                    processNextDestination();
+                if (elevators[closestElevator].destinationQueue.length === 0) {
+                    processNextDestination(elevators[closestElevator], queue);
                 }
             });
         });
 
-        elevator.on("idle", function() {
-            processNextDestination();
+        // Process the next destination for each elevator when it becomes idle
+        elevators.forEach(function(elevator, i) {
+            elevator.on("idle", function() {
+                var queue = queues[i];
+                processNextDestination(elevator, queue);
+            });
         });
 
-        //if a customer press a button inside the elevator, add the floor to the queue if it's not already there
-        elevator.on("floor_button_pressed", function(floorNum) {
-            if (!internalQueue.includes(floorNum)) {
-                internalQueue.push(floorNum);
-            }
-            if (elevator.destinationQueue.length === 0) {
-                processNextDestination();
-            }
+        // Add a floor to the appropriate elevator's internal queue when a customer presses a button inside the elevator
+        elevators.forEach(function(elevator, i) {
+            elevator.on("floor_button_pressed", function(floorNum) {
+                var queue = queues[i];
+                if (!queue.internal.includes(floorNum)) {
+                    queue.internal.push(floorNum);
+                }
+                if (elevator.destinationQueue.length === 0) {
+                    processNextDestination(elevator, queue);
+                }
+            });
         });
-        
-        //if the elevator is passing a floor where a stop is needed, stop there
-        elevator.on("passing_floor", function(floorNum, direction) {
-            if (internalQueue.includes(floorNum)) {
-                elevator.goToFloor(floorNum, true);
-            }
-         });
 
-
+        // Stop at a floor if an elevator is passing a floor where a stop is needed
+        elevators.forEach(function(elevator, i) {
+            elevator.on("passing_floor", function(floorNum, direction) {
+                var queue = queues[i];
+                if (queue.internal.includes(floorNum)) {
+                    elevator.goToFloor(floorNum, true);
+                }
+            });
+        });
     },
+
     update: function(dt, elevators, floors) {
         // We normally don't need to do anything here
     }
